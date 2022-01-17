7bbe363e45578f0f034159c5b79f99f631903d58
Abdullah <abdlhofficial@gmail.com>
2022-01-17 08:37:21 +0700

    17/01/2022

---
+++ app/src/main/java/com/blogspot/codesgram/myintentapp/MoveActivity.java
@@ -0,0 +1,16 @@
+package com.blogspot.codesgram.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+
+}
